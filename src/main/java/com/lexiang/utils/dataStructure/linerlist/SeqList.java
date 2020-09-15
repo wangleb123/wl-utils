9a7bcc5eef26c878de1d0ac8f57a2791fafadc3e
王乐 <1660104869@qq.com>
2020-09-15 11:44:57 +0800

    线性表的顺序存储结构实现

--- src/main/java/com/lexiang/utils/dataStructure/linerlist/SeqList.java
+++ src/main/java/com/lexiang/utils/dataStructure/linerlist/SeqList.java
@@ -1,10 +1,11 @@
 package com.lexiang.utils.dataStructure.linerlist;
-
-import org.apache.commons.lang3.StringUtils;
-
-import javax.jws.Oneway;
 import java.util.*;
 
+
+/**
+ * 线性表的顺序存储结构实现
+ * @param <T>
+ */
 public class SeqList<T>{
 
 
@@ -16,23 +17,24 @@ public class SeqList<T>{
     /**
      * 线性表默认默认空间大小
      */
-    private static final Integer DEFAULT_SIZE = 10;
-
-    /**
-     * 线性表目前插入所在位置
-     */
-    private static int position;
+    private static final int DEFAULT_SIZE = 10;
 
 
     /**
-     * 线性表目前插入所在位置
+     * 超出线性表空间扩展内存大小
      */
-    private static int length;
+    private static final int EXTEND_SIZE = 10;
+
 
     /**
-     * 空线性表
+     * 线性表数据长度
      */
-    private static final Object[] DEFAULT_DATA = {};
+    private int length;
+
+    /**
+     * 线性表内存大小
+     */
+    private int size;
 
 
     public SeqList(){
@@ -41,75 +43,134 @@ public class SeqList<T>{
 
     public SeqList(int initialSIze){
         if(initialSIze >= 0 ){
-            elementData = new Object[initialSIze];
+            this.elementData = new Object[initialSIze];
         }else {
             throw new IllegalArgumentException("线性表长度不能小于0");
         }
-        position = -1;
-        length = initialSIze;
+        this.length = 0;
+        this.size = initialSIze;
     }
 
-    public int size() {
-        return elementData.length;
+    /**
+     * @return 线性表数据长度
+     */
+    public int dataSize() {
+        return this.length;
     }
 
+    /**
+     *
+     * @return 线性表内存大小
+     */
+    public int size(){
+        return this.size;
+    }
+
+    /**
+     * @return 线性表是否为空表
+     */
     public boolean isEmpty() {
-        return position == 0;
+        return this.length == 0;
     }
 
 
+    /**
+     *
+     * @param o 数据
+     * @return 线性表中是否存在该数据
+     */
     public boolean contains(Object o) {
         return indexOf(o).size() != 0;
     }
 
-    public boolean add(Object o) {
-        elementData[++position] = o;
-        return true;
+    /**
+     * 添加数据
+     * @param o 数据
+     */
+    public void add(Object o) {
+        extendElement();
+        this.elementData[this.length++] = o;
     }
 
+    /**
+     * 清空线性表
+     */
     public void clear() {
-        elementData = null;
-        position = -1;
-        elementData = new Object[length];
+        this.elementData = new Object[DEFAULT_SIZE];
+        this.length = 0;
     }
 
+    /**
+     * 获取指定下标的数据
+     * @param index 下标
+     * @return 下标对应的数据
+     */
     public T get(int index) {
-        if(elementData.length <=0 ){
-            throw new ArrayIndexOutOfBoundsException("线性表已空");
-        }else if(elementData.length-1 < index){
-            throw new ArrayIndexOutOfBoundsException("线性表["+index+"]位置已没有元素");
+        if(this.length ==0 ){
+            return null;
+            //throw new ArrayIndexOutOfBoundsException("线性表已空");
+        }else if(this.length-1 < index){
+            return null;
+            //throw new ArrayIndexOutOfBoundsException("线性表["+index+"]位置已没有元素");
+        }else {
+            return (T)elementData[index];
         }
-        return (T)elementData[index];
     }
 
+    /**
+     * 移除元素
+     * @param index 地址下标
+     * @return
+     */
     public T remove(int index) {
-        Object data = elementData[index];
-        for (int i = index; i < elementData.length-1; i++) {
-            elementData[i] = elementData[i+1];
+
+        if(index > this.length -1){
+            return null;
+        }else {
+            Object data = this.elementData[index];
+            for (int i = index; i < this.length-1; i++) {
+                this.elementData[i] = this.elementData[i+1];
+            }
+            // 将index之后的数据的地址引用向前移动一个
+            // 如果线性表表满，最后一个元素的地址引用赋值给前一个
+            // 那么最后一个元素仍然持有原来线性表的最后一个元素的地址引用
+            // 会导致数据重复
+            // 这里就将最后一个元素的地址引用置空
+            this.elementData[this.length-1] = null;
+            this.length -- ;
+            return (T)data;
         }
-        elementData[position] = null;
-        position -- ;
-        return (T)data;
+
     }
 
-    public String toString() {
-        return "SeqList{" +
-                "elementData=" + Arrays.toString(elementData) +
-                '}';
+    public void insert(int index,Object o){
+        extendElement();
+        if(index > this.length -1 ){
+            this.elementData[length++] = o;
+        }else {
+            for (int i = index; i < this.length; i++) {
+                this.elementData[i+1] = this.elementData[i];
+            }
+            this.elementData[index] = o;
+        }
+        this.length++;
+
     }
+
+
 
     public ArrayList<Integer> indexOf(Object o){
         ArrayList<Integer> indexOf = new ArrayList<>();
         if(o == null){
-            for (int i = 0; i < elementData.length; i++) {
-                if (elementData[i] == null) {
+            for (int i = 0; i < this.length; i++) {
+                if (this.elementData[i] == null) {
                     indexOf.add(i);
                 }
 
             }
         }else {
-            for (int i = 0; i < position+1; i++) {
-                if (elementData[i].equals(o)) {
+            for (int i = 0; i < this.length; i++) {
+                if (this.elementData[i].equals(o)) {
                     indexOf.add(i);
                 }
             }
@@ -117,17 +178,45 @@ public class SeqList<T>{
         return indexOf;
     }
 
+
+    /**
+     * 线性表已经超出，默认添加EXTEND_SIZE个内存空间
+     */
+    private void extendElement(){
+        if(this.length == this.size){
+            this.size =  this.size + EXTEND_SIZE;
+            Object[] extend = new Object[this.size];
+            for (int i = 0; i < this.length; i++) {
+                extend[i] = this.elementData[i];
+            }
+            this.elementData = extend;
+        }
+    }
+
+    public String toString() {
+        return "SeqList{" +
+                "数据=" + Arrays.toString(this.elementData) +"\n"+
+                "内存大小"+ this.size+"\n"+
+                "数据量"+ this.length+"\n"+
+                '}';
+    }
+
+
     public static void main(String[] args) throws Exception{
         SeqList<String> linearList = new SeqList<>();
-        linearList.add("123");
-        linearList.remove(0);
-        linearList.clear();
-        linearList.add("234");
-        linearList.add("234");
-        linearList.add("234");
-        linearList.size();
+        for (int i = 0; i < 20; i++) {
+            linearList.add(Integer.toString(i));
+        }
+        linearList.add("22");
+        //linearList.remove(0);
+        //linearList.clear();
+
+        //linearList.remove(3);
         linearList.isEmpty();
-        System.out.println(linearList.indexOf("234"));
+        System.out.println(linearList.toString());
+
+        SeqList<String> linearList1 = new SeqList<>();
+        System.out.println(linearList1.toString());
 
     }
 }
